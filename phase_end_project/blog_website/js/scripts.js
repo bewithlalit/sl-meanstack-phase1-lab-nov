26676252b8daaca45d50de51c0530d92343e4b33
lalit <bewithlalit@gmail.com>
2021-12-16 00:54:19 +0530

    create post using local storage

--- phase_end_project/blog_website/js/scripts.js
+++ phase_end_project/blog_website/js/scripts.js
@@ -5,11 +5,12 @@
 */
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
+    onPageInit();
     const mainNav = document.getElementById('mainNav');
     const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
+        if (currentTop < scrollPos) {
             // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
@@ -27,3 +28,51 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+
+function onPageInit() {
+    if (localStorage.getItem('data') == null) {
+        console.log('on page Init works here')
+        document.querySelector('.post-preview').innerHTML = `<h1>No blogs posted yet</h1>`
+    }
+    else {
+        let info = JSON.parse(localStorage.getItem('data'));
+        info.forEach((i) => {
+            document.querySelector('.post-preview').innerHTML += ` <a href="post.html">
+            <h2 id="postTitle" class="post-title">${i.title}</h2>
+            <h3 id="postMainContent" class="post-subtitle">${i.mainContent}</h3>
+        </a>
+        <p class="post-meta">
+            Posted by
+            <a href="#!">${i.postedBy}</a>
+            on September 24, 2021
+        </p>
+        <hr class="my-4" />`
+        })
+    }
+}
+
+let onSubmit = document.querySelector('#subButton')
+onSubmit.addEventListener('click', () => {
+    let postedBy = document.querySelector('#postedBy').value;
+    let title = document.querySelector('#blogTitle').value;
+    let content = document.querySelector('#blogContent').value;
+    let mainContent = document.querySelector('#blogMainContent').value;
+    if(title == '' && mainContent == ' ' && postedBy == '') {
+        /* document.querySelector('.post-preview').innerHTML = `<h1>No blogs posted yet</h1>` */
+        return;
+    }
+    if (localStorage.getItem('data') == null) {
+        console.log('Nothing here')
+        let info = [{ title: title, content: content, mainContent: mainContent, postedBy: postedBy }]
+        localStorage.setItem('data', JSON.stringify(info));
+    } else {
+        let info = JSON.parse(localStorage.getItem('data'));
+        info.push({ title: title, content: content, mainContent: mainContent, postedBy: postedBy})
+        console.log(info);
+        localStorage.setItem('data', JSON.stringify(info));
+    }
+    window.location.href="index.html";
+})
+
+
+
